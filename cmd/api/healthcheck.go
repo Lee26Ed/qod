e687c38a0799db4754d9a33c06193bd0954f0285
LeePanti <lee.14.panti@gmail.com>
2025-08-25 14:52:18 -0600

    encoding json response v2

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string {
+                                "status": "available",
+                                "environment": app.config.env,
+                                "version": version,
+                              }
+
+	jsResponse, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	
+    jsResponse = append(jsResponse, '\n')
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsResponse)
+
+}
